06df317ca92de56a6a5a60cb11183fb733d9eacd
adelgado <adelgado@bancodeoccidente.com.co>
2023-03-20 20:04:41 -0500

    lambda, apigateway and dynamoDB creation

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -1,8 +1,40 @@
-import {Stack, StackProps} from 'aws-cdk-lib'
+import {Stack, StackProps} from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { Code, Function as LambdaFunction, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { join } from 'path';
+import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
+import { GenericTable } from './GenericTable';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 
 export class SpaceStack extends Stack{
+
+  private api = new RestApi(this, 'SpaceApi');
+  private spaceTable = new GenericTable(
+    'SpacesTable',
+    'SpaceId',
+    this
+  );
+
   constructor(scope: Construct, id:string, props: StackProps) {
     super(scope, id, props)
+
+    /* Without Node Lambda
+    const helloLambda = new LambdaFunction(this, 'helloLambda', {
+      runtime: Runtime.NODEJS_14_X,
+      code: Code.fromAsset(join(__dirname, '..', 'services', 'hello')),
+      handler: 'hello.main'
+    })*/
+
+    const helloLambdaNodeJs = new NodejsFunction(this, 'helloLambdaNodeJs', {
+      entry: (join(__dirname, '..', 'services', 'node-lambda', 'hello.ts')),
+      handler: 'handler'
+    })
+
+    // hello api lambda integration:
+    const helloLambdaIntegration = new LambdaIntegration(helloLambdaNodeJs);
+    const helloLambdaResource = this.api.root.addResource('hello');
+    helloLambdaResource.addMethod('GET', helloLambdaIntegration);
+
+
   }
 }
